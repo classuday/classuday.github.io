f6a0acc675623789acbbd04a3960a132176e5c6b
classuday <realme.uday33@gmail.com>
2026-02-04 05:10:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
-const products = {
+function showPopup(title, specs) {
+  document.getElementById("popup-title").innerText = title;
+  document.getElementById("popup-specs").innerText = specs;
+  document.getElementById("popup").classList.remove("hidden");
+}
 
-  gt: [
-    { name: "realme GT 6", specs: "5G | Snapdragon 7+ Gen 3 | 120Hz AMOLED | 5500mAh" },
-    { name: "realme GT 6T", specs: "5G | Snapdragon 7s Gen 2 | 120Hz AMOLED | 5500mAh" }
-  ],
+function closePopup() {
+  document.getElementById("popup").classList.add("hidden");
+}
 
-  number: [
-    { name: "realme 13 Pro+", specs: "5G | 200MP OIS | Curved AMOLED | 5200mAh" },
-    { name: "realme 13 Pro", specs: "5G | 50MP OIS | AMOLED | 5200mAh" }
-  ],
-
-  c: [
-    { name: "realme C65", specs: "4G | 50MP | 5000mAh" },
-    { name: "realme C67 5G", specs: "5G | 50MP | 5000mAh" }
-  ],
-
-  narzo: [
-    { name: "realme Narzo 70 Pro", specs: "5G | AMOLED | Sony IMX882 | 5000mAh" },
-    { name: "realme Narzo 70", specs: "5G | AMOLED | 5000mAh" }
-  ],
-
-  p: [
-    { name: "realme P1 5G", specs: "5G | AMOLED | 5000mAh" },
-    { name: "realme P1 Pro 5G", specs: "5G | Curved AMOLED | 5000mAh" }
-  ],
-
-  pad: [
-    { name: "realme Pad 2", specs: "11.5 inch | Helio G99 | 8360mAh" }
-  ],
-
-  watch: [
-    { name: "realme Watch S2", specs: "AMOLED | Bluetooth Calling" }
-  ],
-
-  buds: [
-    { name: "realme Buds Air 5 Pro", specs: "ANC | Hi-Res Audio | LDAC" }
-  ]
+const map = {
+  gt: "gt-series",
+  number: "number-series",
+  c: "c-series",
+  narzo: "narzo-series",
+  p: "p-series",
+  pad: "pad-series",
+  watch: "watch-series",
+  buds: "buds-series"
 };
+
+function loadProducts() {
+  for (let key in products) {
+    const box = document.getElementById(map[key]);
+    if (!box) continue;
+
+    products[key].forEach(p => {
+      const div = document.createElement("div");
+      div.className = "card";
+      div.innerText = p.name;
+      div.dataset.name = p.name.toLowerCase();
+      div.onclick = () => showPopup(p.name, p.specs);
+      box.appendChild(div);
+    });
+  }
+}
+
+document.getElementById("search").addEventListener("keyup", function () {
+  const val = this.value.toLowerCase();
+  document.querySelectorAll(".card").forEach(card => {
+    card.style.display = card.dataset.name.includes(val) ? "block" : "none";
+  });
+});
+
+loadProducts();
