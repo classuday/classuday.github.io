dcb2098ec7cbeef71d46fefb3c85130ebac49b0e
classuday <realme.uday33@gmail.com>
2026-02-04 05:09:51 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,69 +1,39 @@
 const products = {
+
   gt: [
-    {
-      name: "realme GT 6",
-      specs: "5G | Snapdragon 7+ Gen 3 | 120Hz AMOLED | 5500mAh"
-    },
-    {
-      name: "realme GT 6T",
-      specs: "5G | Snapdragon 7s Gen 2 | 120Hz AMOLED | 5500mAh"
-    }
+    { name: "realme GT 6", specs: "5G | Snapdragon 7+ Gen 3 | 120Hz AMOLED | 5500mAh" },
+    { name: "realme GT 6T", specs: "5G | Snapdragon 7s Gen 2 | 120Hz AMOLED | 5500mAh" }
   ],
 
   number: [
-    {
-      name: "realme 13 Pro+",
-      specs: "5G | 200MP OIS | Curved AMOLED | 5200mAh"
-    },
-    {
-      name: "realme 13 Pro",
-      specs: "5G | 50MP OIS | AMOLED | 5200mAh"
-    }
+    { name: "realme 13 Pro+", specs: "5G | 200MP OIS | Curved AMOLED | 5200mAh" },
+    { name: "realme 13 Pro", specs: "5G | 50MP OIS | AMOLED | 5200mAh" }
   ],
 
   c: [
-    {
-      name: "realme C65",
-      specs: "4G | 50MP | 5000mAh"
-    },
-    {
-      name: "realme C67 5G",
-      specs: "5G | 50MP | 5000mAh"
-    }
+    { name: "realme C65", specs: "4G | 50MP | 5000mAh" },
+    { name: "realme C67 5G", specs: "5G | 50MP | 5000mAh" }
   ],
 
   narzo: [
-    {
-      name: "realme Narzo 70 Pro",
-      specs: "5G | Sony IMX882 | AMOLED | 5000mAh"
-    }
+    { name: "realme Narzo 70 Pro", specs: "5G | AMOLED | Sony IMX882 | 5000mAh" },
+    { name: "realme Narzo 70", specs: "5G | AMOLED | 5000mAh" }
   ],
 
   p: [
-    {
-      name: "realme P1 5G",
-      specs: "5G | AMOLED | 5000mAh"
-    }
+    { name: "realme P1 5G", specs: "5G | AMOLED | 5000mAh" },
+    { name: "realme P1 Pro 5G", specs: "5G | Curved AMOLED | 5000mAh" }
   ],
 
   pad: [
-    {
-      name: "realme Pad 2",
-      specs: "11.5 inch | Helio G99 | 8360mAh"
-    }
+    { name: "realme Pad 2", specs: "11.5 inch | Helio G99 | 8360mAh" }
   ],
 
   watch: [
-    {
-      name: "realme Watch S2",
-      specs: "AMOLED | Bluetooth Calling"
-    }
+    { name: "realme Watch S2", specs: "AMOLED | Bluetooth Calling" }
   ],
 
   buds: [
-    {
-      name: "realme Buds Air 5 Pro",
-      specs: "ANC | Spatial Audio | Hi-Res"
-    }
+    { name: "realme Buds Air 5 Pro", specs: "ANC | Hi-Res Audio | LDAC" }
   ]
 };
