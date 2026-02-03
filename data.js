17e533e05b308c38c7f68bc43bf5ee6ff9977651
classuday <realme.uday33@gmail.com>
2026-02-04 04:38:31 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+const products = {
+  gt: [
+    {
+      name: "realme GT 6",
+      specs: "5G | Snapdragon | 120Hz Display | 5500mAh"
+    }
+  ],
+  number: [
+    {
+      name: "realme 12 Pro+",
+      specs: "5G | 200MP Camera | Curved Display"
+    }
+  ],
+  c: [
+    {
+      name: "realme C55",
+      specs: "4G | 64MP Camera | 5000mAh"
+    }
+  ]
+};
