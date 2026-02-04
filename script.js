9000ee4f9ed56231e02c45036cef4bd557a32878
classuday <realme.uday33@gmail.com>
2026-02-04 05:43:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,117 @@
-function showPopup(title, specs) {
-  document.getElementById("popup-title").innerText = title;
-  document.getElementById("popup-specs").innerText = specs;
-  document.getElementById("popup").classList.remove("hidden");
-}
+const products = {
 
-function closePopup() {
-  document.getElementById("popup").classList.add("hidden");
-}
+  gt: [
+    {
+      name: "realme GT 6",
+      image: "images/gt6.png",
+      specs: `
+Network: 5G
+Processor: Snapdragon 7+ Gen 3
+Display: 6.78" AMOLED, 120Hz, 6000 nits
+Camera: 50MP OIS (Rear), 32MP (Front)
+Battery: 5500mAh, 120W Fast Charge
+RAM/ROM: 8GB/12GB + 256GB
+Colors: Green, Silver
+`
+    },
+    {
+      name: "realme GT 6T",
+      image: "images/gt6t.png",
+      specs: `
+Network: 5G
+Processor: Snapdragon 7s Gen 2
+Display: AMOLED, 120Hz
+Camera: 50MP (Rear), 16MP (Front)
+Battery: 5500mAh, 120W
+RAM/ROM: 8GB/12GB
+`
+    }
+  ],
 
-const map = {
-  gt: "gt-series",
-  number: "number-series",
-  c: "c-series",
-  narzo: "narzo-series",
-  p: "p-series",
-  pad: "pad-series",
-  watch: "watch-series",
-  buds: "buds-series"
+  number: [
+    {
+      name: "realme 13 Pro+",
+      image: "images/13proplus.png",
+      specs: `
+Network: 5G
+Camera: 200MP OIS
+Display: Curved AMOLED, 120Hz
+Processor: Snapdragon 7s Gen 2
+Battery: 5200mAh
+`
+    }
+  ],
+
+  c: [
+    {
+      name: "realme C67 5G",
+      image: "images/c67.png",
+      specs: `
+Network: 5G
+Processor: Dimensity 6100+
+Camera: 50MP
+Battery: 5000mAh
+`
+    }
+  ],
+
+  narzo: [
+    {
+      name: "realme Narzo 70 Pro",
+      image: "images/narzo70pro.png",
+      specs: `
+Network: 5G
+Display: AMOLED
+Camera: Sony IMX882
+Battery: 5000mAh
+`
+    }
+  ],
+
+  p: [
+    {
+      name: "realme P1 Pro 5G",
+      image: "images/p1pro.png",
+      specs: `
+Network: 5G
+Display: Curved AMOLED
+Battery: 5000mAh
+`
+    }
+  ],
+
+  pad: [
+    {
+      name: "realme Pad 2",
+      image: "images/pad2.png",
+      specs: `
+Display: 11.5 inch
+Processor: Helio G99
+Battery: 8360mAh
+`
+    }
+  ],
+
+  watch: [
+    {
+      name: "realme Watch S2",
+      image: "images/watchs2.png",
+      specs: `
+Display: AMOLED
+Feature: Bluetooth Calling
+`
+    }
+  ],
+
+  buds: [
+    {
+      name: "realme Buds Air 5 Pro",
+      image: "images/budsair5pro.png",
+      specs: `
+ANC: Yes
+Audio: Hi-Res + LDAC
+`
+    }
+  ]
+
 };
-
-function loadProducts() {
-  for (let key in products) {
-    const box = document.getElementById(map[key]);
-    if (!box) continue;
-
-    products[key].forEach(p => {
-      const div = document.createElement("div");
-      div.className = "card";
-      div.innerHTML = `
-  <img src="${p.image}" alt="${p.name}">
-  <p>${p.name}</p>
-`;
-      div.dataset.name = p.name.toLowerCase();
-      div.onclick = () => showPopup(p.name, p.specs);
-      box.appendChild(div);
-    });
-  }
-}
-
-document.getElementById("search").addEventListener("keyup", function () {
-  const val = this.value.toLowerCase();
-  document.querySelectorAll(".card").forEach(card => {
-    card.style.display = card.dataset.name.includes(val) ? "block" : "none";
-  });
-});
-
-loadProducts();
